e0be5577a74a92fa13d34b463dd9bf4e3a42948f
Ashton Cummings <acummings@colsa.com>
2018-09-20 09:26:31 -0500

    Added error checking for when an invalid string is input.

--- index.js
+++ index.js
@@ -63,6 +63,8 @@ function parseTimestring (string, returnUnit, opts) {
 
       totalSeconds += getSeconds(value, unit, unitValues)
     })
+  } else {
+    throw new Error(`The string [${string}] is invalid for timestring`)
   }
 
   if (returnUnit) {
